a2e415c9e7ba7615f8043a161668275e5c5f41c2
veronica pupo <veronica.pupo@hotmail.com>
2022-09-07 15:42:35 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,21 @@
+
+import Header from "./components/Header"
+import SalesCard from "./components/SalesCard"
 function App() {
-  return(
-    <h1>Olá Mundo!</h1>
+  return (
+    <>
+      <Header />
+
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard />
+
+          </div>
+        </section>
+      </main>
+    </>
+
   )
 }
 
