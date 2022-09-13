dc667d36bed5d1787dbd581f5162cf6b62b612d8
Verônica Pupo <105063311+veronicapupo@users.noreply.github.com>
2022-09-12 21:09:34 -0300

    refac impl (#1)

--- backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SmsService.java
@@ -12,37 +12,39 @@ import org.springframework.stereotype.Service;
 import java.text.DecimalFormat;
 
 @Service
-    public class SmsService {
+public class SmsService {
 
-        @Value("${twilio.sid}")
-        private String twilioSid;
+    @Value("${twilio.sid}")
+    private String twilioSid;
 
-        @Value("${twilio.key}")
-        private String twilioKey;
+    @Value("${twilio.key}")
+    private String twilioKey;
 
-        @Value("${twilio.phone.from}")
-        private String twilioPhoneFrom;
+    @Value("${twilio.phone.to}")
+    private String twilioPhoneTo;
+    @Value("${twilio.ms.service.id}")
+    private String twilioMsId;
+    @Autowired
+    private SaleRepository saleRepository;
 
-        @Value("${twilio.phone.to}")
-        private String twilioPhoneTo;
-        @Autowired
-        private SaleRepository saleRepository;
-        public void sendSms(Long saleId) {
+    public void sendSms(Long saleId) {
+        // Find your Account Sid and Token at twilio.com/console
+        Sale sale = saleRepository.findById(saleId).get();
+        String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
 
-            Sale sale = saleRepository.findById(saleId).get();
-            String date = sale.getDate().getMonthValue() + "/" + sale.getDate().getYear();
+        String msg = sale.getSellerName() + " é destaque em " + date
+                + " com total de R$ " + sale.getAmount();
 
-            String msg = "O vendedor " + sale.getSellerName() + " foi destaque em " + date
-                    + " com um total de R$ " + new DecimalFormat("#,##0.00").format(sale.getAmount());
+        Twilio.init(twilioSid, twilioKey);
 
-            Twilio.init(twilioSid, twilioKey);
+        PhoneNumber to = new PhoneNumber(twilioPhoneTo);
 
-            PhoneNumber to = new PhoneNumber(twilioPhoneTo);
-            PhoneNumber from = new PhoneNumber(twilioPhoneFrom);
-
-            Message message = Message.creator(to, from, msg).create();
-
-            System.out.println(message.getSid());
-        }
+        Message message = Message.creator(
+                        to,
+                        twilioMsId,
+                        msg)
+                .create();
+        System.out.println(message.getSid());
     }
+}
 
